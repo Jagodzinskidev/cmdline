ed1bd66361157a72931dcd3b020f3c0d641b96fb
student <student@p.lodz.pl>
2025-05-05 13:27:37 +0200

    added adding functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
 #include <stdio.h>
-int main(int argc, char const *argv[]) {
-    return 0;
+#include <stdlib.h>
+#include <string.h>
+
+void usage(char *program_name) {
+    printf("Sposob uzycia: %s <liczba> <dzialanie> <powtorzenia>\n", program_name);
+    exit(1);
+}
+int main(int argc, char *argv[]) {
+    int i, firstNumber, secondNumber;
+
+    if (argc < 4)
+        usage(argv[0]);
+    firstNumber = atoi(argv[1]);
+    secondNumber = atoi(argv[3]);
+    char *choice = argv[2];
+
+    if (strcmp(choice, "add") == 0) {
+        printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
+    }
 }
