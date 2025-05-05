aedaf9eac3c986dcb26c00797929d1f86b18271a
student <student@p.lodz.pl>
2025-05-05 13:41:53 +0200

    added division functionality

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ void usage(char *program_name) {
 }
 int main(int argc, char *argv[]) {
     int i, firstNumber, secondNumber;
-
-    if (argc < 4)
-        usage(argv[0]);
     firstNumber = atoi(argv[1]);
     secondNumber = atoi(argv[3]);
     char *choice = argv[2];
+
+    if (argc < 4)
+        usage(argv[0]);
 
     if (strcmp(choice, "add") == 0) {
         printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
@@ -23,5 +23,11 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(choice, "mul") == 0) {
         printf("%d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
+    }
+    else if (strcmp(choice, "div") == 0) {
+        if (secondNumber == 0) {
+            printf("Nie mozna dzielic przez 0");
+        }
+        printf("%d / %d = %f", firstNumber, secondNumber, (double)firstNumber/(double)secondNumber);
     }
 }
