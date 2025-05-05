62675da6050876d1e0ab68f05908c98d766118d0
student <student@p.lodz.pl>
2025-05-05 14:47:29 +0200

    changed ifs to a switch

--- main.cpp
+++ main.cpp
@@ -10,25 +10,32 @@ int main(int argc, char *argv[]) {
     int i, firstNumber, secondNumber;
     firstNumber = atoi(argv[1]);
     secondNumber = atoi(argv[3]);
-    char *choice = argv[2];
+    char choice = argv[2][0];
 
-    if (argc < 4)
+    if (argc < 4) {
         usage(argv[0]);
+    } else {
+        printf("Dostepne dzialania: add, sub, mul, div \n");
+    }
 
-    if (strcmp(choice, "add") == 0) {
-        printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
-    }
-    else if (strcmp(choice, "sub") == 0) {
-        printf("%d - %d = %d", firstNumber, secondNumber, firstNumber-secondNumber);
-    }
-    else if (strcmp(choice, "mul") == 0) {
-        printf("%d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
-    }
-    else if (strcmp(choice, "div") == 0) {
-        if (secondNumber == 0) {
-            printf("Nie mozna dzielic przez 0");
-        } else {
-            printf("%d / %d = %f", firstNumber, secondNumber, (double)firstNumber/(double)secondNumber);
-        }
+    switch (choice) {
+        case 'a':
+            printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
+            break;
+        case 's':
+            printf("%d - %d = %d", firstNumber, secondNumber, firstNumber-secondNumber);
+            break;
+        case 'm':
+            printf("%d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
+            break;
+        case 'd':
+            if (secondNumber == 0) {
+                printf("Nie mozna dzielic przez 0");
+            } else {
+                printf("%d / %d = %.2f", firstNumber, secondNumber, (double)firstNumber/(double)secondNumber);
+            }
+            break;
+        default:
+            printf("Nie rozpoznano dzialania");
     }
 }
